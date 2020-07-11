7815df1ef303e02d4ea0429c7d360bb47d6e1942
t.kach <t.kach@mail.ru>
2020-07-11 16:51:19 +0300

    Upload files

---
+++ classes.py
@@ -0,0 +1,53 @@
+class newsContent:
+    
+    def __init__(self, masterPath):
+        self.content = []
+        self.msPath = masterPath
+        self.fileName = 'lastlink.txt'
+        
+    def setContent(self, cont):
+        self.content = cont
+        
+    def getData(self, keyw, index=0):
+        if keyw == 'href':
+            return self.msPath + self.content[index]['href']
+        elif keyw == 'image':
+            return self.content[index]['image']
+        return self.content[index][str(keyw)].text
+    
+    # def getLink(self, index=0):
+    #     return self.content[index]['href']
+    
+    # def writeLinkToFile(self):
+    #     f = open(self.fileName, 'w')
+    #     f.write(self.msPath + self.content['href'])
+    #     f.close()
+
+    def writeThisLinksToFile(self, links):
+        f = open(self.fileName, 'w')
+        for line in links:
+            f.write(line + '\n')
+        f.close()
+        
+    # def getLastLink(self):
+    #     f = open(self.fileName, 'r')
+    #     ln = f.readline()
+    #     f.close()
+    #     return ln
+
+    def getAllLinksFromFile(self):
+        with(open(self.fileName, 'r')) as f:
+            ln = [line.rstrip() for line in f]
+            return ln
+
+
+    # def isEqualLinks(self, index=0):
+    #     f = open(self.fileName, 'r')
+    #     lastLink = f.readline()
+    #     f.close()
+    #     return lastLink == self.msPath + self.content[index]['href']
+    
+    def getStrData(self, index=0):
+        fullLink = self.msPath + self.content[index]['href']
+        return str('Заголовок:\n'+self.content[index]['header'].text+'\nОписание:\n'+self.content[index]['description'].text+'\nСсылка: '+fullLink)
+        
---
+++ gmbot.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+#Created on Wed Jul  1 01:06:14 2020, @author: dj
+
+#___IMPORTS___
+import requests
+from bs4 import BeautifulSoup as BF
+import time
+import discord
+from discord import utils
+from discord.ext import commands
+from xml.etree import ElementTree as ET
+from classes import newsContent
+import datetime
+#___END IMPORTS___
+
+
+HEADERS = {'_user': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+
+configRoot = ET.parse('config.xml').getroot()       # getting the root of the configuration file
+_CONSOLE_OUTPUT =  True if int(configRoot.find('consoleout').text) != 0 else False
+token = str(configRoot.find('token').text)          # getting bot token
+channelID = int(configRoot.find('channelID').text)  # getting channel ID for bot messages
+checkPeriod = int(configRoot.find('period').text)   # getting check period
+newsCount = int(configRoot.find('newscount').text)  # getting the number of news items to check
+
+targets = ET.parse('targets.xml').getroot()         # getting the root of the target file
+currentTarget = targets[0].find('pathURL').text     # getting the first target
+mainPath = str(targets[0].find('mainPath').text)    # getting the site address. the site uses relative links
+
+hd = newsContent(targets[0].find('mainPath').text)
+
+#___BOT INICIALIZATION___
+bot = commands.Bot(command_prefix='!')
+
+def getDataFromSite():
+    response = requests.get(currentTarget, headers = HEADERS)
+    return BF(response.content, 'html.parser')
+    
+#___PARSE FUNCTION___
+def parse():
+    soup = getDataFromSite()
+
+    foundTags = soup.findAll('div', class_ = 'teaser-item')
+    newsBlocks = []
+
+    for item in range(0, newsCount):
+        newsBlocks.append({
+            'header': foundTags[item].find('h2', class_ = 'pos-title').find('a').text,
+            'href': mainPath + foundTags[item].find('h2', class_ = 'pos-title').find('a').get('href'),
+            'description': foundTags[item].find('li', class_ = 'element element-textarea element-textareapro first last').text,
+            'image': foundTags[item].find('img').get('data-src')
+        })
+    
+    hd.setContent(newsBlocks)   
+#___END PARSE FUNCTION___
+
+
+#___BOT FUNCTIONS___
+@bot.event
+async def on_ready():
+    if _CONSOLE_OUTPUT:
+        print('Bot started...')
+    channel = bot.get_channel(channelID)
+    if _CONSOLE_OUTPUT:
+        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),' Check news...')
+    parse()
+    while(True):
+        oldLinks = hd.getAllLinksFromFile()
+        for link in hd.content:
+            if link['href'] not in oldLinks:
+                if oldLinks:
+                    oldLinks.insert(0, link['href'])
+                    if len(oldLinks) > newsCount:
+                        oldLinks.pop()
+                else:
+                    oldLinks.append(link['href'])
+
+                emb = discord.Embed(title = 'Новости игровых распродаж и халявы.', colour = discord.Color.green())
+                emb.set_author(name = bot.user.name, icon_url = bot.user.avatar_url)
+                emb.add_field(name = link['header']+'\n', value = link['description'])
+                emb.set_image(url = link['image'])
+
+                await channel.send(embed = emb)
+
+        hd.writeThisLinksToFile(oldLinks)
+        time.sleep(checkPeriod)
+        if _CONSOLE_OUTPUT:
+            print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),' Check news...')
+        parse()
+
+#___END BOT FUNCTIONS___
+
+
+#___MAIN START___
+if (getDataFromSite()):
+    if _CONSOLE_OUTPUT:
+        print('Connection with site - OK.')
+    bot.run(token)
+else:
+    if _CONSOLE_OUTPUT:
+        print('Connection FAILED.')
+
+
+
+
+
+
